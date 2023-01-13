0b35dff051c626c98fe9c5fcfe4520aca91a2723
yusaemi <dokodemoemi@gmail.com>
2023-01-13 22:50:06 +0800

    ⬆️ Upgrade Spring-Boot 3
    - Spring-Boot
        - 2.7.7 -> 3.0.1
    - Spring-Cloud
        - 2021.0.1 -> 2022.0.0
    - package javax -> jakarta
    - Remove
        - querydsl

--- src/main/java/idv/module/repository/ProductDao.java
+++ src/main/java/idv/module/repository/ProductDao.java
@@ -3,7 +3,6 @@ package idv.module.repository;
 
 import idv.module.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 /**
  * ProductDao. 2020/8/18 上午 10:34
@@ -11,6 +10,6 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
  * @author sero
  * @version 1.0.0
  **/
-public interface ProductDao extends JpaRepository<Product, Integer>, QuerydslPredicateExecutor<Product> {
+public interface ProductDao extends JpaRepository<Product, Integer> {
 
 }
